ba1f4e4675a3d95f81175373efdeaa715d854f97
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-02 11:13:38 +0300

    f

---
+++ exesice13/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+anyNumberText = Convert.ToString(anyNumber);
+(любойNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+еще {
+  Console.WriteLine("-> первых цифр нет");
+}
