c299f0e660b1a5f8034efab2f47c2193c6a69f05
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-02 14:10:04 +0300

    ok

--- exesice13/Program.cs
+++ exesice13/Program.cs
@@ -1,4 +1,4 @@
-﻿int n = int.Parse(Console.ReadLine());
-int k = (int)Math.Log10(n)-2;
-Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
-Console.ReadLine();
+﻿Console.WriteLine("Enter numbers");
+int numbers= int.Parse(Console.ReadLine());
+int threenumber = numbers %10;
+Console.WriteLine (threenumber);
