db68c8c0fb6b962eb0b188ee4956bf282c1c80a4
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-03 15:35:37 +0300

    add 23

---
+++ exesice23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Enter numbers"); 
+int num = int.Parse(Console.ReadLine());
+if (num>0)
+{
+int count = 1;
+while (count<num)
+{
+ Console.Write(count*count*count+",");
+ count++;
+}
+Console.WriteLine(count*count*count+".");
+}
