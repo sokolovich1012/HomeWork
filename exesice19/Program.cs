31779f66a5131297c0119592ebf2945d7573346f
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-03 14:55:47 +0300

    add 19

---
+++ exesice19/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter five-digit number");
+            string num = Console.ReadLine();
+            string rev = string.Concat(num.Reverse());
+            Console.WriteLine(num == rev ? "yes" : "no");
