9da266da762e531d520040dcf23d40bc0c154771
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-07 13:40:37 +0300

    add 29

--- exesice29/Program.cs
+++ exesice29/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Enter numbers masive");
+﻿Console.Write("Enter the number of elements in the array:");
 int elementsCount = int.Parse(Console.ReadLine());
 int [] arrey= new int[elementsCount];
 for (int i = 0; i < arrey.Length; i++)
@@ -6,6 +6,7 @@ for (int i = 0; i < arrey.Length; i++)
     Console.Write("Enter number masiv:/t");
     arrey[i]=int.Parse(Console.ReadLine());
 }
+Console.Write("[");
 for (int i = 0; i < arrey.Length; i++)
 {
     Console.Write(",");
