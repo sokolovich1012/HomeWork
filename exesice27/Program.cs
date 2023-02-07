44592279422b2416c1e3a9e4fe0afd5663657130
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-07 10:34:16 +0300

    add 27

---
+++ exesice27/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Enter numbers");
+int num = int.Parse(Console.ReadLine());
+int a = 0;
+while (num>0)
+{
+ a = a + num %10; 
+ num = num / 10;
+}
+ Console.WriteLine (a);
+
