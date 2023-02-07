cb39a1047e2dceb23a3ab118f06f7ab7f768b01f
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-07 12:07:18 +0300

    add 25

---
+++ exesice25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter one number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter two number");
+int b = int.Parse(Console.ReadLine());
+int c = Pow(a,b);
+int Pow(int num, int n)
+{
+	int res = num;
+	for (int i = 1; i < n; i++)
+	{
+		res *= num;
+	}
+	return res;
+}
+Console.WriteLine(c);
