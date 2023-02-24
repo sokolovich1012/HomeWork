e879d2a34de368df647dbc097c762f5279d23bf2
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-24 09:25:12 +0300

    add 66

---
+++ exesice66/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = InputInt("Enter M:");
+int n = InputInt("Enter N:");
+Console.WriteLine($"sum number on {m} to {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
