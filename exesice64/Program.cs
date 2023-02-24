33db068eb50f589f26a9313c77616b2117b1b95e
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-24 09:11:47 +0300

    add 64

---
+++ exesice64/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = InputInt("Enter numbers");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("введите положительное");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
