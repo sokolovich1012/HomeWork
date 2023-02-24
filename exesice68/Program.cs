7e34896dbcfeacf7cf08052935a15ede916a4b89
sokolovich1012 <sokolovich1.99@mail.ru>
2023-02-24 09:30:43 +0300

    add 68

---
+++ exesice68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter two numbers: M и N.");
+int m = InputInt("Enter M: ");
+int n = InputInt("Enter N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
